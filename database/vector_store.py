2b5571f39e032a7d38f967086e1a566802293d0d
ARCHIT-TEJAS-NEVAGI <architnevagi14920@gmail.com>
2026-03-06 19:31:51 +0530

    updated transformer

--- database/vector_store.py
+++ database/vector_store.py
@@ -1,17 +1,17 @@
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
 
 
 def get_vector_store():
 
     embeddings = HuggingFaceEmbeddings(
-        model_name="all-MiniLM-L6-v2"
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
     )
 
     vector_store = Chroma(
         collection_name="webintel",
         embedding_function=embeddings,
-        persist_directory="./chroma_db"
+        persist_directory="./vector_db"
     )
 
     return vector_store
